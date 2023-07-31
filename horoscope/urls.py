10e19b20a9a45071ebd37742dd937e96decb9df0
chernik1 <vvvvf-vvv@mail.ru>
2023-07-31 10:00:27 +0300

    add new sample

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -5,7 +5,7 @@ from horoscope import views
 urlpatterns = [
     path('type/', views.type),
     path('type/<str:sign_zodiac_element>', views.sign_zodiac_element),
-    path('', views.index),
+    path('', views.index, name='horoscope-index'),
     path('<int:sign_zodiac_number>/', views.zodiac_by_number),
     path('<str:sign_zodiac>/', views.zodiac, name = 'horoscope-name'),
     path('<int:month>/<int:day>', views.zodiac_by_date)
