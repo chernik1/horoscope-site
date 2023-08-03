c208f8ffc67cbc73996edeb995755dcd98951e55
chernik1 <vvvvf-vvv@mail.ru>
2023-08-03 23:36:43 +0300

    add favicon.ico

--- main_tab/urls.py
+++ main_tab/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from main_tab import views
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('', views.tab),
 ]
