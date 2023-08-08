67f1f030a85b5ecc52e46c58543916f05e064a04
chernik1 <vvvvf-vvv@mail.ru>
2023-08-08 16:25:46 +0300

    .

--- first/settings.py
+++ first/settings.py
@@ -18,8 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+#vq91e(#ktpvsh8t+1zqm$!eikt-1e1f+udw9gme6levvqbqv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- main_tab/tests.py
+++ main_tab/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestMain(TestCase):
+
+    def test_main_tab(self):
+        responce = self.client.get('')
+        self.assertEqual(responce.status_code, 200)
